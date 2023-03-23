cd07d16654c9ec04abdb83a2fe16d6cbae4a7a6d
nATOr <gowluonade@gmail.com>
2023-03-23 18:27:41 -0500

    _

--- src/app/hooks/useLocaleStorage.js
+++ src/app/hooks/useLocaleStorage.js
@@ -4,7 +4,8 @@ export default function useHook (key, initialState) {
   const [state, setState] = useState(initialState)
   useEffect(() => {
     const item = localStorage.getItem(key)
-    item && setState(JSON.parse(item))
+    const tasks = JSON.parse(item)
+    item && setState(tasks)
   }, [key])
   useEffect(() => state.length > 0 && localStorage.setItem(key, state), [key, state])
   return [state, setState]

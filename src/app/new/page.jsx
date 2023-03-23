6a784df32aa961832e178c697fb4391c44e4399d
nATOr <gowluonade@gmail.com>
2023-03-23 12:16:52 -0500

    dinamic Form

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,6 +1,17 @@
+'use client'
+import { useState } from 'react'
+
 function Page () {
+  const [task, setTask] = useState()
+  const handleChange = (e) =>
+    setTask({ ...task, [e.target.name]: e.target.value })
+
   return (
-    <div>New</div>
+    <form className='grid'>
+      <input name='title' type='text' placeholder='Title' onChange={handleChange} />
+      <textarea name='description' placeholder='Description' onChange={handleChange} />
+      <button>Save</button>
+    </form>
   )
 }
 export default Page

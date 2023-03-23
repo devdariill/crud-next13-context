959d7dade7310eb8e4823d7744689b4ae826e2c3
nATOr <gowluonade@gmail.com>
2023-03-23 12:21:43 -0500

    handleSubmit

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -5,9 +5,13 @@ function Page () {
   const [task, setTask] = useState()
   const handleChange = (e) =>
     setTask({ ...task, [e.target.name]: e.target.value })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(task)
+  }
 
   return (
-    <form className='grid'>
+    <form className='grid' onSubmit={handleSubmit}>
       <input name='title' type='text' placeholder='Title' onChange={handleChange} />
       <textarea name='description' placeholder='Description' onChange={handleChange} />
       <button>Save</button>
